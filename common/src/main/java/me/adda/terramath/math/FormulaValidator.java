d0015086cb5d943346186692e2d4ff30b55fc05f
Tape Dispenser <gramiakshaye@gmail.com>
2025-04-02 10:11:48 -0600

    missed cot and sec in FormulaValidator.java

--- common/src/main/java/me/adda/terramath/math/FormulaValidator.java
+++ common/src/main/java/me/adda/terramath/math/FormulaValidator.java
@@ -24,7 +24,7 @@ public class FormulaValidator {
             "sinh", "cosh", "tanh", "sqrt", "cbrt", "pow",
             "ln", "lg", "abs", "exp", "floor", "ceil",
             "round", "sign", "gamma", "erf", "beta", "mod",
-            "max", "min", "sigmoid", "clamp", "csc"
+            "max", "min", "sigmoid", "clamp", "csc", "cot", "sec"
     ));
 
     public record ValidationResult(boolean isValid, String errorKey, Object... errorArgs) {
