11f25a1741fa50c5c1b382b421c5f19ddd5ecf45
addavriance <61160742+addavriance@users.noreply.github.com>
2025-04-30 01:47:53 +0300

    chore: clean up and integrate leftover code fragments

--- common/src/main/java/me/adda/terramath/events/TerraMathEvents.java
+++ common/src/main/java/me/adda/terramath/events/TerraMathEvents.java
@@ -3,6 +3,7 @@ package me.adda.terramath.events;
 import me.adda.terramath.api.FormulaCacheHolder;
 import me.adda.terramath.api.TerrainFormulaManager;
 import me.adda.terramath.api.TerrainSettingsManager;
+import me.adda.terramath.config.ModConfig;
 import me.adda.terramath.world.TerrainData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.storage.LevelResource;
@@ -35,15 +36,21 @@ public class TerraMathEvents {
             data.setDirty();
         } else {
             data.applyToManagers();
+
+            FormulaCacheHolder.resetCache();
         }
     }
 
     public static void onLevelUnload(ServerLevel level) {
-        TerraFormulaManager.getInstance().setFormula("");
+        TerrainFormulaManager.getInstance().setFormula("");
         TerrainSettingsManager manager = TerrainSettingsManager.getInstance();
         FormulaCacheHolder.resetCache();
 
         manager.resetToDefaults();
         manager.setUseDensityMode(false);
+
+        if (ModConfig.get().useDefaultFormula) {
+            ModConfig.updateTerrainSettingsFromConfig();
+        }
     }
 }
