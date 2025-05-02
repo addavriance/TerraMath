850434acfabd2704e62cd2ecf1aeb7cc229cf418
addavriance <61160742+addavriance@users.noreply.github.com>
2025-05-03 01:21:10 +0300

    refactor: remove dependency on Architectury API

--- fabric/src/main/java/me/adda/terramath/fabric/events/FabricEvents.java
+++ fabric/src/main/java/me/adda/terramath/fabric/events/FabricEvents.java
@@ -1,13 +1,18 @@
 package me.adda.terramath.fabric.events;
 
-import dev.architectury.event.events.common.LifecycleEvent;
 import me.adda.terramath.events.PlatformEvents;
 import me.adda.terramath.events.TerraMathEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 
 public class FabricEvents implements PlatformEvents {
     @Override
     public void registerEvents() {
-        LifecycleEvent.SERVER_LEVEL_LOAD.register(TerraMathEvents::onLevelLoad);
-        LifecycleEvent.SERVER_LEVEL_UNLOAD.register(TerraMathEvents::onLevelUnload);
+        ServerWorldEvents.LOAD.register((server, world) -> {
+            TerraMathEvents.onLevelLoad(world);
+        });
+
+        ServerWorldEvents.UNLOAD.register((server, world) -> {
+            TerraMathEvents.onLevelUnload(world);
+        });
     }
 }
