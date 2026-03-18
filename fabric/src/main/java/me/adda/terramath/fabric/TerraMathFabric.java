9d199a17c20b02c410862eb61a987cd18d2671b8
addavriance <61160742+addavriance@users.noreply.github.com>
2026-03-19 00:04:44 +0300

    feat: density function

--- fabric/src/main/java/me/adda/terramath/fabric/TerraMathFabric.java
+++ fabric/src/main/java/me/adda/terramath/fabric/TerraMathFabric.java
@@ -1,11 +1,20 @@
 package me.adda.terramath.fabric;
 
 import me.adda.terramath.TerraMath;
+import me.adda.terramath.worldgen.TerraMathDensityFunction;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 
 public class TerraMathFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        Registry.register(
+                BuiltInRegistries.DENSITY_FUNCTION_TYPE,
+                new ResourceLocation(TerraMath.MOD_ID, "terrain"),
+                TerraMathDensityFunction.CODEC.codec()
+        );
         TerraMath.init();
     }
 }
