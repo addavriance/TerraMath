bf9cd673f5188ee076d45c6dc3582259652eaebe
addavriance <61160742+addavriance@users.noreply.github.com>
2026-03-19 00:04:44 +0300

    feat: density function

--- forge/src/main/java/me/adda/terramath/forge/TerraMathForge.java
+++ forge/src/main/java/me/adda/terramath/forge/TerraMathForge.java
@@ -1,26 +1,33 @@
 package me.adda.terramath.forge;
 
+import com.mojang.serialization.Codec;
 import me.adda.terramath.TerraMath;
+import me.adda.terramath.worldgen.TerraMathDensityFunction;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.DensityFunction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(TerraMath.MOD_ID)
 public class TerraMathForge {
+
+    private static final DeferredRegister<Codec<? extends DensityFunction>> DENSITY_FUNCTION_TYPES =
+            DeferredRegister.create(Registries.DENSITY_FUNCTION_TYPE, TerraMath.MOD_ID);
+
+    @SuppressWarnings("unused")
+    private static final RegistryObject<Codec<? extends DensityFunction>> TERRAIN_DENSITY_FUNCTION =
+            DENSITY_FUNCTION_TYPES.register("terrain", TerraMathDensityFunction.CODEC::codec);
+
     public TerraMathForge() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        registerForgeEvents(modEventBus);
+        DENSITY_FUNCTION_TYPES.register(modEventBus);
 
         ForgeConfigRegistration.register();
 
         TerraMath.init();
-    }
-
-    private void registerForgeEvents(IEventBus eventBus) {
-        // MyBlocks.BLOCKS.register(eventBus);
-        // MyItems.ITEMS.register(eventBus);
-
-        // eventBus.register(SomeEventHandler.class);
     }
 }
